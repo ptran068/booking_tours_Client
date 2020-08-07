257fae0926661b14344d07897a4685d43fdbefb0
ptran068 <ptran068@gmail.com>
2020-08-07 10:25:30 +0700

    update

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,24 +1,26 @@
 import axios from 'axios'
 import $store from '../store'
 import $router from '../router'
-
+import BaseService from './base.service'
 import { API_URL } from '../.env'
-
-/**
- ******************************
- * @API
- ******************************
- */
 
 export async function login({ email, password }) {
   const user = await axios.post(`${API_URL}/user/login`, { email, password})
   _setAuthData({token: user.data.token, user: user.data.user})
+
   return user
 }
 
 export async function create({ email, phone, password }) {
   const user = await axios.post(`${API_URL}/user/create`, { email, phone, password})
-  print(user)
+  _setAuthData({token: user.data.token, user: user.data.user})
+ 
+  return user
+}
+
+export async function getUserByToken() {
+  const token = getToken()
+  const user = await request({auth: true}).post(`${API_URL}/user/decode-token`, { token })
   return user
 }
 
@@ -40,3 +42,17 @@ function _resetAuthData() {
   $store.commit("user/SET_CURRENT_USER", {});
   localStorage.setItem("token","");
 }
+
+class UserService extends BaseService {
+  get entity() {
+    return 'user'
+  }
+
+  getUserByToken() {
+    const token = getToken()
+    const user = this.request({auth: true}).post(`/${this.entity}/decode-token`, { token })
+    return user
+  }
+}
+
+export default new UserService()
